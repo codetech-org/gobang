6fdfbd28f53efd72626693b5bf7ff9879f781617
jimin <807782317@qq.com>
2021-11-06 12:43:01 +0800

    feat: add web page

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == '__main__':
+    import os
+    import uvicorn
+
+    ct_env = os.getenv("CT_ENV", "dev")
+    if ct_env == "prod":
+        uvicorn.run(app, host="0.0.0.0")  # noqa
+    else:
+        uvicorn.run(app)  # noqa
